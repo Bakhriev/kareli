11f1b675fef59ad22be1b7a2412ff16b089d0650
Bakhriev <muradbakhriev@gmail.com>
2024-03-20 14:51:48 +0500

    fix cumulative layout shift

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,13 +2,6 @@ const burger = document.querySelector('.burger');
 const mobileMenu = document.querySelector('.header__nav');
 const overlay = document.querySelector('.overlay');
 
-const moveElement = (elem, where, order = 999) => {
-	const destination = document.querySelector(where);
-	const toTransfer = document.querySelector(elem);
-
-	destination.insertBefore(toTransfer, destination.children[order]);
-};
-
 burger.addEventListener('click', () => {
 	burger.classList.toggle('active');
 	mobileMenu.classList.toggle('active');
@@ -19,18 +12,6 @@ overlay.addEventListener('click', () => {
 	burger.classList.remove('active');
 	mobileMenu.classList.remove('active');
 	overlay.classList.remove('active');
-});
-
-if (window.innerWidth <= 992) {
-	moveElement('.header__actions', '.header__nav');
-}
-
-window.addEventListener('resize', () => {
-	if (window.innerWidth <= 992) {
-		moveElement('.header__actions', '.header__nav');
-	} else {
-		moveElement('.header__actions', '.header__container');
-	}
 });
 
 const swiper = new Swiper('.hero__slider', {
